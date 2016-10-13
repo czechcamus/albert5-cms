9ef103a10f0905ccaccfc9d472d8ea764b733fd0
czechcamus <czechcamus@gmail.com>
2016-10-13 15:37:31 +0200

    kancl 13.10.2016, 15:37

--- private/frontend/themes/basic/page/page.php
+++ private/frontend/themes/basic/page/page.php
@@ -13,23 +13,7 @@ $page = $menuContent->content;
 /** @noinspection PhpUndefinedFieldInspection */
 $this->context->layout = 'page-content';
 
-if ($page->image) {
-	echo DisplayImage::widget([
-		'options' => [
-			'class' => 'responsive-img',
-			'title' => $menuContent->title
-		],
-		'category' => 'all',
-		'image' => $page->image->filename
-	]);
-}
-
-if ($page->perex) {
-	echo '<div class="perex">' . $page->perex . '</div>';
-}
-
-if ($page->description) {
-	echo '<div class="description">';
-	echo FrontEndHelper::parseContent($page->description);
-	echo '</div>';
-}
+echo $this->renderFile( '@frontend/themes/basic/components/_content.php', [
+	'menuContent' => $menuContent,
+	'page' => $page
+] );
